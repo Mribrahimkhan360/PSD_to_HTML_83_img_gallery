03d5d535c37e7a3d8d51790084f230338671c3af
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2021-06-22 10:06:50 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,90 @@
+
+$('#img-1').click(function(){
+	var imgsrcValue = $('#img-1').attr('src');
+	$('#mainImage').attr('src',imgsrcValue);
+});
+
+$('#img-2').click(function(){
+	var imgsrcValue = $('#img-2').attr('src');
+	$('#mainImage').attr('src',imgsrcValue);
+});
+$('#img-3').click(function(){
+	var imgsrcValue = $('#img-3').attr('src');
+	$('#mainImage').attr('src',imgsrcValue);
+});
+$('#img-4').click(function(){
+	var imgsrcValue = $('#img-4').attr('src');
+	$('#mainImage').attr('src',imgsrcValue);
+});
+
+
+
+// function createHeadingPragrapElement(){
+// 	var newHeadingElement = document.createElement('h1');
+// 	var newPragraphElement = document.createElement('p');
+
+
+// 	newHeadingElement.innerHTML = "This is Heading";
+// 	newPragraphElement.innerHTML = "This is Prahraph";
+
+// 	document.getElementById('div').appendChild(newHeadingElement);
+// 	document.getElementById('div').appendChild(newPragraphElement);
+
+// }
+
+
+
+
+
+
+
+// document.getElementById('btn').onclick = function (){
+// 	var newHeadingElement = document.createElement('h1');
+// 	var newPragraphElement = document.createElement('p');
+
+
+// 	newHeadingElement.innerHTML = "This is Heading";
+// 	newPragraphElement.innerHTML = "This is Prahraph";
+
+// 	document.getElementById('div').appendChild(newHeadingElement);
+// 	document.getElementById('div').appendChild(newPragraphElement);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('btn').onclick = function (){
+// 	createHeadingPragrapElement()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#firstName').keyup (function(){
+// 	var firstNameValue = $('#firstName').val();
+// 	$('#res1').text(firstNameValue);
+// });
+// $('#lastName').keyup (function(){
+// 	var lastNameValue = $('#lastName').val();
+// 	$('#res2').text(lastNameValue);
+// });
+// $('#lastName').blur (function(){
+// 	var firstNameValue = $('#firstName').val();
+// 	var lastNameValue = $('#lastName').val();
+// 	$('#res3').text(firstNameValue+' '+lastNameValue);
+// });
